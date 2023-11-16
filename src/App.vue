d28d6842c7359433c7d6cfaa782847b62f6e1dd6
smile1346 <kantapon.tantra@mail.kmutt.ac.th>
2023-11-17 02:23:02 +0700

    [Add] init project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="bg-blue-500 text-white p-4">
+    This is a Tailwind CSS styled component.
+  </div>
+
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped>
+
+</style>
