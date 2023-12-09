bdf9ca2a6464cdb50fc955cfa17e6501aae25007
Asamaporn Pladsri <jajah.asmp@gmail.com>
2023-12-10 01:50:11 +0700

    [Update] change navbar

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import { RouterView } from 'vue-router'
 import NavBarStudent from './components/NavBarStudent.vue';
 import NavbarAdmin from './components/NavbarAdmin.vue';
 import NavbarAdvisor from './components/NavbarAdvisor.vue';
+import NavbarAllRole from './components/NavbarAllRole.vue';
 
 </script>
 
 <template>
+  <NavbarAllRole />
   <!-- <NavBarStudent /> -->
-  <NavbarAdmin />
+  <!-- <NavbarAdmin /> -->
   <!-- <NavbarAdvisor /> -->
   <RouterView />
 </template>
