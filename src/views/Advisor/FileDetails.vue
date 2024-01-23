d6a1586f58da5d93db29c7da927b66269796864a
blu333 <72536743+theerutth@users.noreply.github.com>
2024-01-24 04:33:49 +0700

    update_FileDetails

--- src/views/Advisor/FileDetails.vue
+++ src/views/Advisor/FileDetails.vue
@@ -1,19 +1,29 @@
 <script></script>
 <template>
-    <div class="grid grid-cols-2">
-        <div class="bg-white shadow-md p-2 text-sm">
-            ข้อมูลนักศึกษา
+    <div class="grid grid-cols-2 mt-16 ">
+        <div class="bg-white shadow-md p-2 text-sm h-fit w-10/12 space-y-4">
+            <div>ข้อมูลนักศึกษา</div>
+            <hr />
+            <div>รหัสนักศึกษา</div>
+            <div>ชื่อ นามสกุล</div>
+            <div>ชื่อหัวข้อโครงงาน</div>
+            <div>รายวิชาจัดทำ IS</div>
         </div>
-        <div class="bg-white shadow-md p-2 text-sm text-center">
-            filepreview</div>
-    <div class="bg-white shadow-md p-2 text-sm">
-        แสดงความคิดเห็น
+        <div class="bg-white shadow-md p-2 text-sm text-gray-500 text-center h-full flex items-center justify-center">
+            filepreview
+        </div>
+        <div class="bg-white shadow-md p-2 text-sm h-fit w-10/12 space-y-4 mt-10">
+            <div>แสดงความคิดเห็น</div>
+            <hr/>
+            <form>
+                <input type="text" class="w-full rounded-sm border-gray-400 h-24">
+            </form>
+        </div>
+        <div class="bg-white shadow-md p-2 text-sm h-fit space-y-4 mt-10">
+            <div>แบบไฟล์แสดงความคิดเห็น</div>
+        </div>
+        <div class="bg-white shadow-md p-2 text-sm h-fit w-10/12 space-y-4 mt-10">
+            เอกสารบทความ
+        </div>
     </div>
-    <div class="bg-white shadow-md p-2 text-sm">
-       <div>แบบไฟล์แสดงความคิดเห็น</div> 
-    </div>
-    <div class="bg-white shadow-md p-2 text-sm">
-        เอกสารบทความ
-    </div>
-</div>
 </template>
