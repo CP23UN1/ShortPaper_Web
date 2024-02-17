13df1fec59d67aa23b11947c184ecd5a33beaa34
Asamaporn Pladsri <jajah.asmp@gmail.com>
2024-02-18 00:03:27 +0700

    [Update] apiservice

--- src/composables/apiService.js
+++ src/composables/apiService.js
@@ -18,18 +18,35 @@ class ApiService {
     return await api.delete(`/announcement/delete/${announcementId}`)
   }
 
+  // Auth
+  async login(info) {
+    return await api.post('/login', info)
+  }
+
+  // Comments
+  async getComments(fileId) {
+    return await api.get(`/comments/${fileId}`)
+  }
+  async addComment(comment) {
+    return await api.post('/add-comment', comment)
+  }
+
   // Committees
   async getCommittees() {
     return await api.get(`/committees`)
   }
   async addCommittee(file) {
-    return await api.post(`/add-from-csv`, file)
+    return await api.post(`/add-from-csv`, file, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
+  }
+  async assignCommittee(file) {
+    return await api.post(`/assign-from-csv-for-student`, file, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    })
   }
 
   // File
-  async getFiles() {
-    return await api.get(`/files`)
-  }
   async getFileByShortpaper(shortpaperId) {
     return await api.get(`/files/${shortpaperId}`)
   }
@@ -44,8 +61,14 @@ class ApiService {
   async getFileType() {
     return await api.get(`/filetype`)
   }
-  async getFileById(studentId) {
-    return await api.get(`/files/${studentId}`)
+  async getFiles() {
+    return await api.get(`/files`)
+  }
+  async getFilesByStudent(studentId) {
+    return await api.get(`/file-by-student/${studentId}`)
+  }
+  async getFileByTypeAndStudent(typeId, studentId) {
+    return await api.get(`/file-by-id-and-student/${typeId}/${studentId}`)
   }
 
   // Short Paper
@@ -53,7 +76,7 @@ class ApiService {
     return await api.get(`/shortpapers`)
   }
   async searchShortPapers(keyword) {
-    return await api.get(`/shortpapers/${keyword}`)
+    return await api.get(`/shortpaper-by-filter/${keyword}`)
   }
 
   // Students
@@ -61,10 +84,10 @@ class ApiService {
     return await api.get(`/students`)
   }
   async searchStudents(keyword) {
-    return await api.get(`/students/${keyword}`)
+    return await api.get(`/students-by-filter/${keyword}`)
   }
   async getStudentById(studentId) {
-    return await api.get(`/student/${studentId}`)
+    return await api.get(`/student-by-id/${studentId}`)
   }
   async createStudent(studentData) {
     return await api.post(`/student/create`, studentData)
@@ -80,10 +103,8 @@ class ApiService {
   async getSubjects() {
     return await api.get(`/subjects`)
   }
-  // Auth
-  async login(info) {
-    
-    return await api.post('/login', info)
+  async searchSubjects(keyword) {
+    return await api.get(`/subjects-by-filter/${keyword}`)
   }
 }
 export default new ApiService()
