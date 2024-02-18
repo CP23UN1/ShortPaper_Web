57ef687db693847d4ff0f23595b36d1167dda4f9
Asamaporn Pladsri <jajah.asmp@gmail.com>
2024-02-19 02:08:29 +0700

    [Update] login page

--- src/components/ButtonMain.vue
+++ src/components/ButtonMain.vue
@@ -11,8 +11,9 @@ const props = defineProps({
   },
   className: {
     type: String,
-    default: ''
-  }
+    default:
+      'text-white bg-bluebtn hover:bg-blue-800',
+  },
 })
 </script>
 
@@ -20,7 +21,7 @@ const props = defineProps({
   <button
     :type="typeButton"
     :class="className"
-    class="text-white bg-bluebtn hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+    class="focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
   >
     {{ text }}
   </button>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useAuthStore } from '../stores/auth'
 import { useValidateStore } from '../stores/validate'
 import { useRoute, useRouter } from 'vue-router'
+import { Modal } from 'flowbite'
 
 import ButtonMain from '../components/ButtonMain.vue'
 
@@ -14,6 +15,9 @@ const router = useRouter()
 
 const email = ref()
 const password = ref()
+
+const isModalOpen = ref(false)
+let modalInstance = null
 
 const validateData = () => {
   let isValid = true
@@ -36,6 +40,7 @@ const login = async () => {
     if (validateData()) {
       await authStore.login({ email: email.value, password: password.value })
       if (authStore.isLoggedIn == true) {
+        toggleModal()
         router.push('/')
       }
     }
@@ -43,69 +48,32 @@ const login = async () => {
     console.error(err)
   }
 }
+
+const toggleModal = () => {
+  isModalOpen.value = !isModalOpen.value
+}
+
+onMounted(() => {
+  const targetEl = document.getElementById('description-modal')
+  modalInstance = new Modal(targetEl)
+})
 </script>
 
 <template>
-  <div
-    class="flex flex-col lg:flex-row items-center justify-center h-screen bg-gray-200"
-    style="
-      background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-        url('/images/kmutt.png');
-      background-size: cover;
-    "
-  >
-    <!-- Left Side: Description -->
-    <div class="lg:w-1/2">
-      <div class="max-w-2xl mx-auto p-8 bg-white rounded-lg shadow-md">
-        <div class="mb-4">
-          <h1 class="text-bluemain font-bold">กำหนดการ</h1>
-          <ol>
-            <li>
-              1. ถ้าไม่ส่ง IS Report
-              ตามกําหนดการที่อาจารย์ผู้สอนกําหนดครั้งเป็นต้นไป
-              กรรมการสงวนสิทธิ์ในการอ่าน นักศึกษาอาจต้องลงทะเบียนเรียนใหม่
-            </li>
-            <li>
-              2. การ Upload แต่ละครั้งให้แนบสรุปประเด็นที่ต้องแก้ไขครั้งก่อนด้วย
-            </li>
-          </ol>
-        </div>
-
-        <div>
-          <h1 class="text-bluemain font-bold">ขั้นตอนการอัปโหลด IS Report</h1>
-          <ol>
-            <li>
-              1. อัปโหลดไฟล์นามสกุล pdf เท่านั้น โดยตั้งชื่อไฟล์ตามรหัสนักศึกษา
-              เช่น 6644xxxxxxx.pdf เป็นต้น
-            </li>
-            <li>
-              2. ให้รวมไฟล์ให้อยู่ในรูปแบบไฟล์เดียว ไม่ต้องแยกบท หรือเนื้อหา
-            </li>
-            <li>
-              3. ตรวจสอบอีเมล และเบอร์โทรศัพท์ว่าถูกต้องหรือไม่ เนื่องจาก
-              ความเห็น IS Report จะส่งกลับทางอีเมล
-            </li>
-            <li>
-              4. ถ้าต้องการเปลี่ยนแปลงแก้ไขอีเมล หรือเบอร์โทรศัพท์ สามารถ
-              แก้ไขได้ที่หน้าแก้ไขข้อมูล
-            </li>
-            <li>
-              5. ถ้าไม่สามารถอัปโหลดไฟล์ หรือใช้งานระบบไม่ได้ ให้ส่งไฟล์ IS
-              Report มาที่ webadmin@sit.kmutt.ac.th พร้อมทั้งแจ้งอีเมล
-              และเบอร์โทร ติดต่อกลับ
-            </li>
-            <li>
-              6. ในการเข้าใช้งานระบบเป็นครั้งแรก ให้นักศึกษากรอกข้อมูลส่วนตัว
-              และข้อมูลของโครงงานให้ครบถ้วน
-            </li>
-          </ol>
-        </div>
-      </div>
-    </div>
-
-    <!-- Right Side: Login Form -->
-    <div class="lg:w-1/2">
+  <div class="flex items-center justify-center h-screen bg-gray-200">
+    <!-- Login Form -->
+    <div class="w-3/4">
       <div class="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-md">
+        <div>
+          <img
+            src="/images/SIT-LOGO.png"
+            alt="SIT-logo"
+            width="300"
+            class="mx-auto"
+          />
+          <hr class="border-t my-4" />
+        </div>
+
         <form @submit.prevent="login" class="flex flex-col items-center">
           <div class="mb-4 w-full">
             <label for="email" class="block font-bold mb-2">อีเมล</label>
@@ -131,13 +99,103 @@ const login = async () => {
           </div>
           <ButtonMain
             type-button="submit"
-            class-name="w-52"
+            class-name="w-52 bg-white text-bluemain hover:bg-bluemain hover:text-white outline outline-2 outline-bluemain"
             text="เข้าสู่ระบบ"
           />
         </form>
         <!-- <div class="text-right mt-1">
           <a href="#" class="text-bluemain text-sm">Forgot Password?</a>
         </div> -->
+        <div class="mt-4">
+          <!-- <p
+            class="text-sm text-bluemain underline text-center cursor-pointer"
+            @click="toggleModal"
+          >
+            กำหนดการ และขั้นตอนการอัปโหลด IS Report
+          </p> -->
+          <p
+            class="text-sm text-bluemain underline text-center cursor-pointer"
+            @click="toggleModal"
+          >
+            กำหนดการ และขั้นตอนการอัปโหลด IS Report
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Description Modal -->
+  <div
+    class="fixed top-0 flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50"
+    :class="{ hidden: !isModalOpen }"
+    id="description-modal"
+  >
+    <button
+      type="button"
+      class="absolute top-[17rem] right-1/3"
+      @click="toggleModal"
+    >
+      <svg
+        class="w-3 h-3"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 14 14"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+        />
+      </svg>
+      <span class="sr-only">ปิดหน้าต่าง</span>
+    </button>
+
+    <div class="max-w-2xl mx-auto p-8 bg-white rounded-lg shadow-md">
+      <div class="mb-4">
+        <h1 class="text-bluemain font-bold">กำหนดการ</h1>
+        <ol>
+          <li>
+            1. ถ้าไม่ส่ง IS Report
+            ตามกําหนดการที่อาจารย์ผู้สอนกําหนดครั้งเป็นต้นไป
+            กรรมการสงวนสิทธิ์ในการอ่าน นักศึกษาอาจต้องลงทะเบียนเรียนใหม่
+          </li>
+          <li>
+            2. การ Upload แต่ละครั้งให้แนบสรุปประเด็นที่ต้องแก้ไขครั้งก่อนด้วย
+          </li>
+        </ol>
+      </div>
+
+      <div>
+        <h1 class="text-bluemain font-bold">ขั้นตอนการอัปโหลด IS Report</h1>
+        <ol>
+          <li>
+            1. อัปโหลดไฟล์นามสกุล pdf เท่านั้น โดยตั้งชื่อไฟล์ตามรหัสนักศึกษา
+            เช่น 6644xxxxxxx.pdf เป็นต้น
+          </li>
+          <li>
+            2. ให้รวมไฟล์ให้อยู่ในรูปแบบไฟล์เดียว ไม่ต้องแยกบท หรือเนื้อหา
+          </li>
+          <li>
+            3. ตรวจสอบอีเมล และเบอร์โทรศัพท์ว่าถูกต้องหรือไม่ เนื่องจาก ความเห็น
+            IS Report จะส่งกลับทางอีเมล
+          </li>
+          <li>
+            4. ถ้าต้องการเปลี่ยนแปลงแก้ไขอีเมล หรือเบอร์โทรศัพท์ สามารถ
+            แก้ไขได้ที่หน้าแก้ไขข้อมูล
+          </li>
+          <li>
+            5. ถ้าไม่สามารถอัปโหลดไฟล์ หรือใช้งานระบบไม่ได้ ให้ส่งไฟล์ IS Report
+            มาที่ webadmin@sit.kmutt.ac.th พร้อมทั้งแจ้งอีเมล และเบอร์โทร
+            ติดต่อกลับ
+          </li>
+          <li>
+            6. ในการเข้าใช้งานระบบเป็นครั้งแรก ให้นักศึกษากรอกข้อมูลส่วนตัว
+            และข้อมูลของโครงงานให้ครบถ้วน
+          </li>
+        </ol>
       </div>
     </div>
   </div>
