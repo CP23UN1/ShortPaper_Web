b2a9bd4ebb30a1b11f986d0191aba5c1a4e13583
Asamaporn Pladsri <jajah.asmp@gmail.com>
2024-03-28 02:42:57 +0700

    [Update] design of add data

--- src/views/AdminRole/AdminAddStudentData.vue
+++ src/views/AdminRole/AdminAddStudentData.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import ApiService from '../../composables/apiService'
 import Header from '../../components/Header.vue'
 import ButtonMain from '../../components/ButtonMain.vue'
+import FileInput from '../../components/FileInput.vue'
 
 const file = ref(null)
 
@@ -10,32 +11,29 @@ const handleFileUpload = (event) => {
   file.value = event.target.files[0]
 }
 
-// Vue component
 const addStudentData = async () => {
   if (file.value) {
     try {
-      await ApiService.addStudent(file.value); // Pass the file directly to the assignCommittee function
-      console.log('File successfully sent.'); // Log success message
+      await ApiService.addStudent(file.value)
     } catch (error) {
-      console.error('Error assigning committee:', error); // Log error message
+      console.error('Error assigning committee:', error)
     }
-  } else {
-    console.error('No file selected.'); // Log error message if no file is selected
   }
 }
-
 </script>
 
 <template>
-    <div>
-      <Header class="text-sm rounded-md" header="Add Student Data" />
-      <div class="p-5 shadow-md text-sm">
-        <input type="file" @change="handleFileUpload" accept=".csv" />
-        <ButtonMain text="Assign Advisors" @click="addStudentData" />
-      </div>
+  <div>
+    <Header class="text-sm rounded-md" header="เพิ่มข้อมูลนักศึกษา" />
+    <div class="p-5 shadow-md text-sm">
+      <input type="file" @change="handleFileUpload" class="ml-2 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none" accept=".csv" />
+      <ButtonMain
+        text="เพิ่มนักศึกษา"
+        @click="addStudentData"
+        class="ml-3 bg-bluemain border hover:bg-white border-bluemain hover:text-bluemain"
+      />
     </div>
+  </div>
 </template>
 
-  
 <style></style>
-  
