8f76d9d5d4a9086438fafcdbc206c343bfe064d2
Asamaporn Pladsri <jajah.asmp@gmail.com>
2024-04-15 22:41:17 +0700

    [Update] show/hide password login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -104,7 +104,12 @@ const toggleErrorModal = () => {
   isErrorModalOpen.value = !isErrorModalOpen.value
 }
 
-onMounted(() => {})
+// Show password
+const passwordVisible = ref(false)
+
+const togglePasswordVisibility = () => {
+  passwordVisible.value = !passwordVisible.value
+}
 </script>
 
 <template>
@@ -142,7 +147,7 @@ onMounted(() => {})
               required
             />
           </div>
-          <div class="mb-4">
+          <!-- <div class="mb-4">
             <label for="password" class="block font-bold mb-2">รหัสผ่าน</label>
             <input
               type="password"
@@ -152,6 +157,62 @@ onMounted(() => {})
               placeholder="รหัสผ่าน"
               required
             />
+          </div> -->
+          <div class="mb-4">
+            <label for="password" class="block font-bold mb-2">รหัสผ่าน</label>
+            <div class="relative">
+              <input
+                :type="passwordVisible ? 'text' : 'password'"
+                id="password"
+                v-model="password"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 border-gray-400 leading-tight focus:outline-none focus:shadow-outline xl:text-lg lg:text-base md:text-sm sm:text-xs"
+                placeholder="รหัสผ่าน"
+                required
+              />
+              <button
+                @click="togglePasswordVisibility"
+                class="absolute inset-y-0 right-0 flex items-center px-3 text-gray-700 focus:outline-none"
+              >
+                <svg
+                  v-if="passwordVisible"
+                  class="w-5 h-5 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M3.933 13.909A4.357 4.357 0 0 1 3 12c0-1 4-6 9-6m7.6 3.8A5.068 5.068 0 0 1 21 12c0 1-3 6-9 6-.314 0-.62-.014-.918-.04M5 19 19 5m-4 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  /></svg
+                ><svg
+                  v-else
+                  class="w-5 h-5 text-gray-800 dark:text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-width="2"
+                    d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </button>
+            </div>
           </div>
           <hr class="my-5 text-black" />
           <div class="flex justify-center items-center">
